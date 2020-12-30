3e2678b7336ab5f9eda69b9117eacd77ab4acf9a
mollymoog <mmkover@gmail.com>
2020-12-30 16:24:11 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h3>Title: {movie.title}</h3>
+            <h5>Time: {movie.time} minutes</h5>
+            <ul>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        ))
+        
+        }
     </div>
   );
 };
